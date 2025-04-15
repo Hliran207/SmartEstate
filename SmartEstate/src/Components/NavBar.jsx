ef81446d0d9970cfcce8bde146db2e7e55687e78
EtaiCohen <etaico@ac.sce.ac.il>
2025-04-15 19:52:24 +0300

    admin page

--- SmartEstate/src/Components/NavBar.jsx
+++ SmartEstate/src/Components/NavBar.jsx
@@ -19,7 +19,83 @@ const NavBar = ({ user, setUser }) => {
     }
   };
 
+  return (import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const NavBar = ({ user, setUser }) => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/logout/",
+        {},
+        { withCredentials: true }
+      );
+      if (response.status === 200) {
+        setUser(null);
+        navigate("/");
+      } else {
+        console.error("Logout failed");
+      }
+    } catch (err) {
+      console.error("Logout failed", err);
+    }
+  };
+
   return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+      <Link to="/" className="navbar-brand">
+        MyApp
+      </Link>
+      <div className="collapse navbar-collapse justify-content-end">
+        <ul className="navbar-nav">
+          {!user ? (
+            <>
+              <li className="nav-item">
+                <Link to="/register" className="nav-link">
+                  Register
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/login" className="nav-link">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/AdminHomePage" className="nav-link">
+                  Admin
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="nav-item">
+                <span className="nav-link">Hi, {user}</span>
+              </li>
+              <li className="nav-item">
+                <Link to="/profile" className="nav-link">
+                  Personal Page
+                </Link>
+              </li>
+              <li className="nav-item">
+                <button
+                  className="btn btn-link nav-link"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </button>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
       <Link to="/" className="navbar-brand">
         MyApp
