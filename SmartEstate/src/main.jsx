1e6baad1680c5efb235f0adba7ac0d8d4eb0c849
lucasker98 <lucaske@ac.sce.ac.il>
2025-04-19 15:44:39 +0300

    Create custom personal survey feature

--- SmartEstate/src/main.jsx
+++ SmartEstate/src/main.jsx
@@ -10,6 +10,7 @@ import Register from "./Components/User/Register.jsx";
 import Login from "./Components/User/LogIn.jsx";
 import UserProfile from "./Components/User/UserProfile.jsx";
 import AdminHomePage from "./Components/Admin/AdminHomePage.jsx";
+import PersonalQuestionnaire from "./Components/User/PersonalQuestionnaire.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -20,6 +21,7 @@ createRoot(document.getElementById("root")).render(
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<UserProfile />} />
         <Route path="/adminHomePage" element={<AdminHomePage />} />
+        <Route path="/questionnaire" element={<PersonalQuestionnaire />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
